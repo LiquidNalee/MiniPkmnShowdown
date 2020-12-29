33de15f13e514a4d34029abaa69c896d5698cc2f
LiquidSame <liquid.same@gmail.com>
2020-12-29 19:38:42 +0100

    Update PokemonBaseModel.py

--- models/pkmn/PokemonBaseModel.py
+++ models/pkmn/PokemonBaseModel.py
@@ -4,13 +4,13 @@ from models.pkmn.types.PokemonType import PokemonType, format_types_tuple, type_
 
 class PokemonBaseModel:
 
-    def __init__(self, name: str, types: (PokemonType, PokemonType), base_stats_dict: StatsDict):
+    def __init__(self, name: str, types: (PokemonType, PokemonType), base_stats: StatsDict):
         self.name = name
         self.type = format_types_tuple(types)
-        self.base_stats_dict = base_stats_dict
+        self.base_stats = base_stats
 
     def __eq__(self, other):
         if self.__class__ != other.__class__:
             return NotImplementedError
         return self.name == other.name and type_equals(self.type, other.type) \
-               and self.base_stats_dict == other.base_stats_dict
+            and self.base_stats == other.base_stats
