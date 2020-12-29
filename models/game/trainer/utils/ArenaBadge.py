e13047cb2bd994550f2e79ae475df33716dde8a6
LiquidSame <liquid.same@gmail.com>
2020-12-29 19:41:06 +0100

    Create ArenaBadge.py

---
+++ models/game/trainer/utils/ArenaBadge.py
@@ -0,0 +1,12 @@
+from enum import Enum
+
+
+class ArenaBadge(Enum):
+    BadgeRoche = 1
+    BadgeCascade = 2
+    BadgeFoudre = 3
+    BadgePrisme = 4
+    BadgeAme = 5
+    BadgeMarais = 6
+    BadgeVolcan = 7
+    BadgeTerre = 8
