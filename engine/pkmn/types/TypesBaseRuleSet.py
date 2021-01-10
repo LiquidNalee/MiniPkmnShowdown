da5bf315162eba89b53eb43e21967601a6e84bc8
LiquidSame <liquid.same@gmail.com>
2021-01-10 16:14:34 +0100

    Created TypeEffectiveness Charts

---
+++ engine/pkmn/types/TypesBaseRuleSet.py
@@ -0,0 +1,17 @@
+from pandas import DataFrame
+from models.pkmn.types.PokemonType import PokemonType
+
+
+class TypesBaseRuleSet:
+
+    # .loc on DataFrame: effectiveness of attack on defense
+    # .iloc on DataFrame: effectiveness of defense on attack
+
+    def __init__(self, type_effectiveness_chart: DataFrame):
+        self.type_effectiveness_chart = type_effectiveness_chart
+
+    def __getitem__(self, item: PokemonType) -> (DataFrame, DataFrame):
+        return self.type_effectiveness_chart.loc(item), self.type_effectiveness_chart.iloc(item)
+
+    def getEffectiveness(self, attack_type: PokemonType, defender_type: PokemonType) -> int:
+        return self.type_effectiveness_chart[attack_type, defender_type]
